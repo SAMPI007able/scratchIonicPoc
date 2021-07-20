b6137ce62b7d31eb756a8e434f05e2f2b2bdaf64
SAMPI007able <lahiri.arnab4@gmail.com>
2021-07-20 23:00:53 +0530

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BrowserTab } from '@ionic-native/browser-tab/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers:[BrowserTab]
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private browserTab: BrowserTab) {
+    // browserTab.isAvailable()
+    // .then(isAvailable => {
+    //   if (isAvailable) {
+    //     browserTab.openUrl('https://ionic.io');
+    //   } else {
+    //     // open URL with InAppBrowser instead or SafariViewController
+    //   }
+    // });
+  }
+
+  ngOnInit(){
+    
+  }
 
 }
